1e6b235e35af50b8af7584a723aa46e1ede3a8a5
MeganTrioMusic <coomott@naver.com>
2023-08-31 04:31:26 +0900

    Other Sound Add

--- Assets/GameInput.cs
+++ Assets/GameInput.cs
@@ -50,7 +50,7 @@ public partial class @GameInput: IInputActionCollection2, IDisposable
                 {
                     ""name"": """",
                     ""id"": ""978bfe49-cc26-4a3d-ab7b-7d7a29327403"",
-                    ""path"": ""<Gamepad>/leftStick"",
+                    ""path"": ""<XInputController>/dpad"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": "";Gamepad"",

059dabfe49a9f41a555ad63badc5b735909ad7b4
MeganTrioMusic <coomott@naver.com>
2023-08-31 00:00:25 +0900

    PlayerInput

--- Assets/2.Scripts/PlayerMove.cs
+++ Assets/2.Scripts/PlayerMove.cs
@@ -1,45 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float cameraSpeed = 5.0f;
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    public float moveSpeed = 30f;
 
-    public GameObject player;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
-        Vector3 dir = player.transform.position - this.transform.position;
-        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
-        this.transform.Translate(moveVector);
+        
     }
-    #region PublicVariables
-    #endregion
 
-    #region PrivateVariables
-    #endregion
-
-    #region PublicMethod
-    public void OnMovement(InputAction.CallbackContext callback)
+    void FixedUpdate()
     {
-        if (!gameObject.activeSelf)
-        {
-            return;
-        }
-        Vector2 move = (callback.ReadValue<Vector2>());
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        rb.velocity = move;
-
-
-        if (callback.canceled)
-        {
-            rb.velocity = new Vector2();
-        }
+        rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
-    #endregion
 
-    #region PrivateMethod
-    #endregion
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
 }

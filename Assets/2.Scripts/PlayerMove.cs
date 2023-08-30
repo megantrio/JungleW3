633b65b8a055fe956eef649eff3f9544303f5812
MeganTrioMusic <coomott@naver.com>
2023-08-30 22:46:29 +0900

    Tutorial

--- Assets/2.Scripts/PlayerMove.cs
+++ Assets/2.Scripts/PlayerMove.cs
@@ -5,6 +5,16 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float cameraSpeed = 5.0f;
+
+    public GameObject player;
+
+    private void Update()
+    {
+        Vector3 dir = player.transform.position - this.transform.position;
+        Vector3 moveVector = new Vector3(dir.x * cameraSpeed * Time.deltaTime, dir.y * cameraSpeed * Time.deltaTime, 0.0f);
+        this.transform.Translate(moveVector);
+    }
     #region PublicVariables
     #endregion
 
@@ -21,6 +31,8 @@ public class PlayerMove : MonoBehaviour
         Vector2 move = (callback.ReadValue<Vector2>());
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         rb.velocity = move;
+
+
         if (callback.canceled)
         {
             rb.velocity = new Vector2();
